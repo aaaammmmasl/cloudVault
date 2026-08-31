dba16a515dbc0440295f8af2231353431ec2170b
aaaammmmasl <kerchaouiamin06@gmail.com>
2026-08-31 22:17:06 +0100

    add some features for s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,30 @@ resource "aws_s3_bucket" "cloudvault" {
     Project = "CloudVault"
   }
 }
+
+resource "aws_s3_bucket_versioning" "cloudvault" {
+  bucket = aws_s3_bucket.cloudvault.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "cloudvault" {
+  bucket = aws_s3_bucket.cloudvault.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "cloudvault" {
+  bucket = aws_s3_bucket.cloudvault.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
