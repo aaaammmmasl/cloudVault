e2feef715a41a0ebf9b66897e053309fc5449c5d
aaaammmmasl <kerchaouiamin06@gmail.com>
2026-09-01 22:03:49 +0100

    add vpc,subnet,igw,rt,sg

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,3 +33,80 @@ resource "aws_s3_bucket_public_access_block" "cloudvault" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+
+resource "aws_vpc" "cloudvault" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name    = "CloudVault"
+    Project = "CloudVault"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.cloudvault.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "CloudVault-public-subnet"
+    Project = "CloudVault"
+  }
+}
+
+resource "aws_internet_gateway" "cloudvault" {
+  vpc_id = aws_vpc.cloudvault.id
+
+  tags = {
+    Name    = "CloudVault-igw"
+    Project = "CloudVault"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.cloudvault.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.cloudvault.id
+  }
+
+  tags = {
+    Name    = "CloudVault-public-route-table"
+    Project = "CloudVault"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "cloudvault" {
+  name        = "cloudvault-sg"
+  description = "Security group for CloudVault EC2"
+  vpc_id      = aws_vpc.cloudvault.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "CloudVault-SG"
+    Project = "CloudVault"
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,7 @@ variable "aws_region" {
   type        = string
   default     = "eu-north-1"
 }
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
